ed38d40b38dac368bcba49b4f1ff714df56e4705
Paras Gupta <parasg1999@gmail.com>
2023-01-07 18:11:45 +0530

    fix: prizes responsive

--- components/PrizesSection.vue
+++ components/PrizesSection.vue
@@ -9,8 +9,8 @@
         align="center"
         direction="column"
       >
-        <CFlex class="cards-grid">
-          <div v-for="(prize, index) in prizes" :key="index">
+        <CFlex class="cards-grid mobile">
+          <CBox v-for="(prize, index) in prizesMobile" :key="index">
             <div :class="['card', prize.class]">
               <div class="image">
                 <img :src="prize.image" :alt="prize.name" />
@@ -27,7 +27,28 @@
                 </p>
               </div>
             </div>
-          </div>
+          </CBox>
+        </CFlex>
+
+        <CFlex class="cards-grid desktop">
+          <CBox v-for="(prize, index) in prizes" :key="index">
+            <div :class="['card', prize.class]">
+              <div class="image">
+                <img :src="prize.image" :alt="prize.name" />
+              </div>
+              <div class="card-side front">
+                <div class="texts">
+                  <h4 class="prizeName">{{ prize.name }}</h4>
+                  <p class="prizeDescription" v-html="prize.details"></p>
+                </div>
+              </div>
+              <div class="card-side back">
+                <p>
+                  <span v-html="prize.description"></span>
+                </p>
+              </div>
+            </div>
+          </CBox>
         </CFlex>
       </CFlex>
     </section>
@@ -35,7 +56,7 @@
 </template>
 
 <script>
-import { CFlex } from '@chakra-ui/vue'
+import { CBox, CFlex } from '@chakra-ui/vue'
 import HeadingVue from './HeadingComponent.vue'
 import ContainerVue from './Container.vue'
 
@@ -44,6 +65,7 @@ export default {
     ContainerVue,
     HeadingVue,
     CFlex,
+    CBox,
   },
   data() {
     return {
@@ -63,6 +85,32 @@ export default {
           class: 'Winner',
           description:
             "<ul><li><span style='color: rgba(233, 83, 34, 1)'>INR 35,000</span> Prize to winning team</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 100</span> Amazon Gift Card from Quicknode.</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 150 </span>of free S3-compatible object storage credits with Filebase</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 449</span> of 1 year of Draftbit Pro</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 375</span> of In-Kind Wolfram Award</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 125 </span>of Digital Ocean Credits</li><li>Hack This Fall <span style='color: rgba(233, 83, 34, 1)'>Swag Pack</span></li><li><span style='color: rgba(233, 83, 34, 1)'>Certificates</span> to members</li><li>Special Mention on <a style='color: rgba(233, 83, 34, 1)'  target='_blank' rel='noopener noreferrer' href='https://twitter.com/hackthisfall'>@hackthisfall</a> socials</li></ul>",
+        },
+        {
+          name: '₹10k',
+          image: require('~/assets/prizes/bronze.svg'),
+          details: '2nd Runner-Up',
+          class: 'Third',
+          description:
+            "<ul><li><span style='color: rgba(233, 83, 34, 1)'>INR 20,000</span> Prize to winning team</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 250 </span>of free S3-compatible object storage credits with Filebase</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 449</span> of 1 year of Draftbit Pro</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 375</span> of In-Kind Wolfram Award</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 100</span> of Digital Ocean Credits</li><li>Hack This Fall <span style='color: rgba(233, 83, 34, 1)'>Swag Pack</span></li><li><span style='color: rgba(233, 83, 34, 1)'>Certificates</span> to members</li><li>Special Mention on <a style='color: rgba(233, 83, 34, 1)'  target='_blank' rel='noopener noreferrer' href='https://twitter.com/hackthisfall'>@hackthisfall</a> socials</li></ul>",
+        },
+      ],
+      prizesMobile: [
+        {
+          name: '₹30k',
+          image: require('~/assets/prizes/gold.svg'),
+          details: 'Winner',
+          class: 'Winner',
+          description:
+            "<ul><li><span style='color: rgba(233, 83, 34, 1)'>INR 35,000</span> Prize to winning team</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 100</span> Amazon Gift Card from Quicknode.</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 150 </span>of free S3-compatible object storage credits with Filebase</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 449</span> of 1 year of Draftbit Pro</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 375</span> of In-Kind Wolfram Award</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 125 </span>of Digital Ocean Credits</li><li>Hack This Fall <span style='color: rgba(233, 83, 34, 1)'>Swag Pack</span></li><li><span style='color: rgba(233, 83, 34, 1)'>Certificates</span> to members</li><li>Special Mention on <a style='color: rgba(233, 83, 34, 1)'  target='_blank' rel='noopener noreferrer' href='https://twitter.com/hackthisfall'>@hackthisfall</a> socials</li></ul>",
+        },
+        {
+          name: '₹20k',
+          image: require('~/assets/prizes/silver.svg'),
+          details: '1st Runner-Up',
+          class: 'Second',
+          description:
+            "<ul><li><span style='color: rgba(233, 83, 34, 1)'>INR 50,000</span> Prize to winning team</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 250</span> Amazon Gift Card from Quicknode.</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 250 </span>of free S3-compatible object storage credits with Filebase</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 449</span> of 1 year of Draftbit Pro</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 375</span> of In-Kind Wolfram Award</li><li><span style='color: rgba(233, 83, 34, 1)'>USD 250</span> of Digital Ocean Credits</li><li>Hack This Fall <span style='color: rgba(233, 83, 34, 1)'>Swag Pack</span></li><li><span style='color: rgba(233, 83, 34, 1)'>Certificates</span> to members</li><li>Special Mention on <a style='color: rgba(233, 83, 34, 1)'  target='_blank' rel='noopener noreferrer' href='https://twitter.com/hackthisfall'>@hackthisfall</a> socials</li></ul>",
         },
         {
           name: '₹10k',
@@ -94,6 +142,19 @@ export default {
     flex-direction: row;
     justify-content: center;
     align-items: center;
+    display: flex;
+
+    &.mobile {
+      display: none;
+    }
+
+    &.desktop {
+      display: flex;
+
+      @include respond-below(sm) {
+        display: none;
+      }
+    }
 
     .card {
       position: relative;
@@ -194,6 +255,64 @@ export default {
 
     .clickable {
       cursor: pointer;
+    }
+
+    @include respond-below(lg) {
+      .card {
+        display: flex;
+        justify-content: center;
+        margin-left: auto;
+        min-width: 16rem;
+      }
+    }
+
+    @include respond-below(md) {
+      .card {
+        display: flex;
+        justify-content: center;
+        margin-left: auto;
+        min-width: 14rem;
+      }
+    }
+
+    @include respond-below(sm) {
+      flex-direction: column;
+      align-items: center;
+
+      &.mobile {
+        display: flex;
+      }
+
+      .card {
+        display: flex;
+        justify-content: center;
+        margin-left: auto;
+        min-width: 85vw;
+
+        &.Second {
+          margin-left: 0;
+        }
+
+        &.Second,
+        &.Third {
+          margin-top: 5rem;
+        }
+
+        &.Winner {
+          margin-top: 0rem;
+        }
+
+        .texts {
+          .prizeName {
+            margin-top: 1rem;
+            font-size: 3rem;
+          }
+
+          .prizeDescription {
+            font-size: 1.5rem;
+          }
+        }
+      }
     }
   }
 }
